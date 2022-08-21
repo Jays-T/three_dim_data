44fd8720130839e4833737eec32c662d6b8cb6a7
John Traas <j.j.h.traas@gmail.com>
2022-08-21 18:27:44 +0000

    add initial layout and dropdown container/selector

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from dash import Dash, html
+from dash_bootstrap_components.themes import BOOTSTRAP
+
+from src.components.layout import create_layout
+
 def main() -> None:
-    pass
+    app = Dash(external_stylesheets=[BOOTSTRAP])
+    app.title = "Three Dim Data"
+    app.layout = create_layout(app)
+    app.run()
 
 
 if __name__ == "__main__":
